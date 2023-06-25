f8ac70cc4ae16b9bdc7f798d77edbb820f530983
a1a2a3a4a5 <linub7@gmail.com>
2023-06-25 14:21:25 +0430

    start project & initial configurations -> done

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const {
+//   signup,
+//   signin,
+//   forgotPassword,
+//   resetPassword,
+//   updatePassword,
+//   signoutUser,
+// } = require('../controllers/auth');
+// const { protect } = require('../middleware/auth');
+
+const router = express.Router();
+
+// router.post('/auth/forgot-password', forgotPassword);
+// router.patch('/auth/reset-password/:token', resetPassword);
+// router.patch('/auth/update-my-password', protect, updatePassword);
+
+// router.post('/auth/signup', signup);
+// router.post('/auth/signin', signin);
+// router.get('/auth/signout', protect, signoutUser);
+
+module.exports = router;
